38d172410c94d3daa5bbb5c941d452fc447bff3f
yeraldycarpio <yeraldycarpio_morales@hotmail.com>
2024-02-22 08:49:58 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoTec02HYCM220224/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoTec02HYCM220224.Models;
+using System.Diagnostics;
+
+namespace ProyectoTec02HYCM220224.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ProyectoTec02HYCM220224/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoTec02HYCM220224.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
